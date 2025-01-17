288ad5b8db59e8c8612a349d06f8818953488516
tulsirampathe <tulsirampathe81@gmail.com>
2025-01-17 21:40:26 +0530

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import { socialMediaLinks } from "../constants";
 
 function Footer() {
   return (
-    <footer className="fixed right-4 bottom-2 z-20 sm:right-2 sm:bottom-2 md:right-4 md:bottom-4 lg:right-8 lg:bottom-6">
+    <footer className="fixed right-1 bottom-2 z-20 sm:right-2 sm:bottom-2 md:right-4 md:bottom-4 lg:right-8 lg:bottom-6">
       <div className="flex flex-col justify-end gap-3 sm:gap-2">
         {socialMediaLinks.map((social, index) => (
           <div
             key={index}
             onClick={() => window.open(social.url, "_blank")}
             aria-label={`Open ${social.name}`}
-            className="relative black-gradient w-14 h-14 sm:w-12 sm:h-12 rounded-full flex justify-center items-center cursor-pointer group transform transition-transform duration-300 hover:scale-110"
+            className="relative black-gradient w-10 h-10 sm:w-12 sm:h-12 rounded-full flex justify-center items-center cursor-pointer group transform transition-transform duration-300 hover:scale-110"
           >
             <img
               src={social.img}
